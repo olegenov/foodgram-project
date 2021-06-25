d065325f75674e5137e562529822cfc8ab604149
olegenov <nik_kitaev@mail.ru>
2021-06-26 02:21:08 +0300

    fix database settings

--- foodgram/foodgram/settings.py
+++ foodgram/foodgram/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
