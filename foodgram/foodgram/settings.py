f454febbce4e341f793f7f3ef77f58788ab027de
olegenov <nik_kitaev@mail.ru>
2021-06-26 02:16:14 +0300

    fix requirements.txt

--- foodgram/foodgram/settings.py
+++ foodgram/foodgram/settings.py
@@ -82,10 +82,6 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        '''
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        '''
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
